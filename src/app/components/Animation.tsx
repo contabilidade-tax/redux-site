51298f32be3d5c6b0a65679194f90e346790b753
José <josepsousa2012@gmail.com>
2023-07-13 11:57:20 -0300

    refactor bg 600

--- src/app/components/Animation.tsx
+++ src/app/components/Animation.tsx
@@ -1,6 +1,9 @@
 'use client'
 import React, { useEffect, useRef } from 'react'
 import gsap from 'gsap'
+import { PixiPlugin } from 'gsap/PixiPlugin'
+
+gsap.registerPlugin(PixiPlugin)
 
 const GameScene: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
@@ -10,20 +13,13 @@ const GameScene: React.FC = () => {
       const canvas = canvasRef.current
       const ctx = canvas.getContext('2d')!
 
-      let cw = (canvas.width = 800)
-      let ch = (canvas.height = 800)
+      let cw = (canvas.width = 500) // Largura do canvas
+      let ch = (canvas.height = 500) // Altura do canvas
 
-      const bg = new Image()
-      bg.src = 'https://i.postimg.cc/fWqT7zSp/bg2.png'
-      const bgPosition = { x: 0 }
-
-      const dino = { img: new Image(), spriteOffsetX: 0, x: cw / 2, top: 402 }
-
-      const dinoImg = new Image()
-      dinoImg.onload = () => {
-        // A imagem está agora totalmente carregada e pode ser usada sem qualquer atraso.
-      }
-      dinoImg.src = 'https://i.postimg.cc/HkV0jQpT/dfhfgfghfghfghh.png'
+      const bg = { img: new Image(), x: 0 }
+      const dino = { img: new Image(), spriteOffsetX: 0, x: cw / 3, top: 300 }
+      bg.img.src = 'https://i.postimg.cc/fWqT7zSp/bg2.png'
+      dino.img.src = 'https://i.postimg.cc/HkV0jQpT/dfhfgfghfghfghh.png'
 
       gsap.to(dino, {
         duration: 0.55,
@@ -38,24 +34,24 @@ const GameScene: React.FC = () => {
 
         ctx.clearRect(0, 0, cw, ch)
 
-        ctx.drawImage(bg, bgPosition.x, 0)
+        ctx.drawImage(bg.img, bg.x, 0, bg.img.width, ch)
         ctx.drawImage(
           dino.img,
           dino.spriteOffsetX,
           0,
           100,
           140,
-          dino.x - 250,
-          dino.top + 150,
+          dino.x,
+          dino.top,
           85,
           150,
         )
       })
-
-      gsap.to(bgPosition, {
-        x: -bg.width,
+      // const speed =
+      const bgTl = gsap.timeline({ repeat: -1 })
+      bgTl.to(bg, {
+        x: -bg.img.width + cw,
         duration: 20,
-        repeat: -1,
         ease: 'none',
       })
     }
