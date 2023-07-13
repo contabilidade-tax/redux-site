da3b873ec88565321d2eaa798c62e030a6223c18
José <josepsousa2012@gmail.com>
2023-07-13 15:17:15 -0300

    teste

--- src/app/components/Animation.tsx
+++ src/app/components/Animation.tsx
@@ -9,16 +9,32 @@ const GameScene: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
   useEffect(() => {
+    // Dentro da função useEffect:
     if (canvasRef.current) {
       const canvas = canvasRef.current
       const ctx = canvas.getContext('2d')!
+      const cw = (canvas.width = 600)
+      const ch = (canvas.height = 600)
 
-      let cw = (canvas.width = 500) // Largura do canvas
-      let ch = (canvas.height = 500) // Altura do canvas
+      const bg = [
+        { img: new Image(), x: 0 },
+        { img: new Image(), x: 0 },
+        { img: new Image(), x: 0 },
+        { img: new Image(), x: 0 },
+        { img: new Image(), x: 0 },
+        { img: new Image(), x: 0 },
+        { img: new Image(), x: 0 },
+      ]
 
-      const bg = { img: new Image(), x: 0 }
+      bg[0].img.src = 'https://i.postimg.cc/0ySBb4f4/bg-1.png'
+      bg[1].img.src = 'https://i.postimg.cc/G2YMpg1Q/bg-2.png'
+      bg[2].img.src = 'https://i.postimg.cc/hPKCS0ZL/bg-3.png'
+      bg[3].img.src = 'https://i.postimg.cc/T16t5jdX/bg-4.png'
+      bg[4].img.src = 'https://i.postimg.cc/1RGChdBR/bg-5.png'
+      bg[5].img.src = 'https://i.postimg.cc/TYQthWBZ/bg-6.png'
+      bg[6].img.src = 'https://i.postimg.cc/6qfYr7q4/bg-7.png'
+
       const dino = { img: new Image(), spriteOffsetX: 0, x: cw / 3, top: 300 }
-      bg.img.src = 'https://i.postimg.cc/fWqT7zSp/bg2.png'
       dino.img.src = 'https://i.postimg.cc/HkV0jQpT/dfhfgfghfghfghh.png'
 
       gsap.to(dino, {
@@ -29,12 +45,21 @@ const GameScene: React.FC = () => {
       })
 
       gsap.ticker.add(() => {
-        cw = canvas.width = 500
-        ch = canvas.height = 500
-
         ctx.clearRect(0, 0, cw, ch)
 
-        ctx.drawImage(bg.img, bg.x, 0, bg.img.width, ch)
+        for (let i = 0; i < bg.length; i++) {
+          const bgImage = bg[i]
+          ctx.drawImage(bgImage.img, bgImage.x, 0, bgImage.img.width, ch)
+        }
+
+        const bgTl = gsap.timeline({ repeat: -1 })
+        bgTl.to(bg, {
+          x: `-=${cw}`,
+          duration: 20,
+          ease: 'none',
+          repeatDelay: 1, // Intervalo entre cada repetição em segundos
+        })
+
         ctx.drawImage(
           dino.img,
           dino.spriteOffsetX,
@@ -46,13 +71,6 @@ const GameScene: React.FC = () => {
           85,
           150,
         )
-      })
-      // const speed =
-      const bgTl = gsap.timeline({ repeat: -1 })
-      bgTl.to(bg, {
-        x: -bg.img.width + cw,
-        duration: 20,
-        ease: 'none',
       })
     }
   }, [])
