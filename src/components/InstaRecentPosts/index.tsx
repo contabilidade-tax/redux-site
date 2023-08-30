2277ba2b294da3ef0d72e1733bb9633fef794eb9
José Pinheiro <josepsousa2012@gmail.com>
2023-08-30 11:27:17 -0300

    insta

---
+++ src/components/InstaRecentPosts/index.tsx
@@ -0,0 +1,18 @@
+interface RecentPostsProps {
+    className?: string;
+}
+
+export default function RecentPosts({ className }: RecentPostsProps) {
+    return (
+        <section className={className}>
+            <iframe
+                src='https://www.juicer.io/api/feeds/hiveoficial_/iframe'
+                frameBorder={0}
+                width='1000'
+                className='h-[90%]'
+                // height='1000'
+                style={{ display: 'block', margin: '0 auto' }}>
+            </iframe>
+        </section >
+    )
+}
