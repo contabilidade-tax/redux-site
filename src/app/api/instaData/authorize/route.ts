384d11d7c90e6c60f0b8ac883df90d866e9adb2a
José Sousa <josepsousa2012@gmail.com>
2023-12-13 01:03:56 -0300

    fix method to get long lived token

--- src/app/api/instaData/authorize/route.ts
+++ src/app/api/instaData/authorize/route.ts
@@ -3,27 +3,30 @@ import { NextRequest, NextResponse } from 'next/server'
 import qs from 'qs'
 
 async function getLongLivedToken(longLivedTokenurl: string, client_secret: string, access_token: string) {
-  const response = await axios.post(
-    longLivedTokenurl,
-    {},
-    {
+  try {
+    const response = await axios.get(longLivedTokenurl, {
       params: { grant_type: "ig_exchange_token", client_secret, access_token }
-    })
-
-  const data = response.data
-  return data
+    });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response?.data.error_message ?? error.message);
+  }
 }
+
 
 async function createInstaToken(apiUrl: string, Instadata: any) {
-  const response = await axios.post(
-    apiUrl,
-    { ...Instadata },
-    {}
-  )
-
-  const data = response.data
-  return data
+  try {
+    const response = await axios.post(
+      apiUrl,
+      { ...Instadata },
+      { params: { key: 'token' } }
+    );
+    return response.data;
+  } catch (error: any) {
+    throw new Error(`${error.response?.data.details ?? error.response?.data} - INTERNO`);
+  }
 }
+
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
@@ -32,7 +35,7 @@ export async function GET(req: NextRequest, res: NextResponse) {
     const apiIgLongLivedTokenUrl = `${process.env.NEXT_PUBLIC_API_IG_URL}/access_token`
     const createTokenApiUrl = `${process.env.NEXT_PUBLIC_HOME}/api/createInstaData`
     const redirect_uri = `${process.env.NEXT_PUBLIC_HOME}/api/instaData/authorize`
-    const client_secret = process.env.NEXT_PUBLIC_API_IG_APP_SECRET
+    const client_secret = process.env.NEXT_PUBLIC_API_IG_APP_SECRET!
     const client_id = process.env.NEXT_PUBLIC_API_IG_APP_ID
     const data = qs.stringify({
       client_id,
@@ -46,27 +49,23 @@ export async function GET(req: NextRequest, res: NextResponse) {
       throw new Error('Code not found, this is a route to authorize app only');
     }
 
-    await axios.post(
-      tokenUrl,
-      data, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    }).then(response => {
-      console.log(response.data)
-      const client_secret = process.env.NEXT_PUBLIC_API_IG_APP_SECRET!
-      const shortLivedToken = response.data.access_token
-
-      const data = getLongLivedToken(apiIgLongLivedTokenUrl, client_secret, shortLivedToken)
-      const createdToken = createInstaToken(createTokenApiUrl, data)
-
-      return NextResponse.redirect("/")
-    }).catch((error: any) => {
-      console.log(error.response!.data)
-      throw new Error(error.response.data.error_message)
-    })
-
-    return NextResponse.json({ message: 'Success' }, { status: 200 });
+    try{
+      const response = await axios.post(
+        tokenUrl,
+        data,
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+      );
+      console.log(response.data);
+    
+      const shortLivedToken = response.data.access_token;
+      const longLivedTokenData = await getLongLivedToken(apiIgLongLivedTokenUrl, client_secret, shortLivedToken);
+      const createdToken = await createInstaToken(createTokenApiUrl, longLivedTokenData);
+    
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_HOME}/home?welcome=1`);
+    } catch (error: any) {
+      console.error(error.response?.data ?? error.message);
+      throw new Error(error.response?.data.error_message ?? error.message);
+    }
 
   } catch (e: any) {
     if (e instanceof Error) {
