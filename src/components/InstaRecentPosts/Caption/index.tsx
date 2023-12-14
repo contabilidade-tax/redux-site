9f872ec8409e0c870e761ae8f933dd4ff21a98e2
José <josepsousa2012@gmail.com>
2023-12-14 17:31:49 -0300

    finalized recentposts

--- src/components/InstaRecentPosts/Caption/index.tsx
+++ src/components/InstaRecentPosts/Caption/index.tsx
@@ -12,8 +12,8 @@ function Caption({ text }: CaptionProps) {
     return (
         <div className="modal">
             <article className="modal-container">
-                <section className="modal-container-body rtf">
-                    <p className='px-2'>{text}</p>
+                <section className="modal-container-body rtf text-center">
+                    <p className='px-3'>{text}</p>
                 </section>
             </article>
         </div>
--- src/components/InstaRecentPosts/index.tsx
+++ src/components/InstaRecentPosts/index.tsx
@@ -72,45 +72,49 @@ function InstaRecentPosts({ className }: InstaRecentPostsProps) {
     }, []);
 
     return (
-        <section className={cn(
-            'w-full h-full flex overflow-x-auto gap-4 py-6',
-            styles.instaPostsWrapper,
-            className
-        )}
-        >
-            {user && <div className="currentUser absolute min-w-52 w-max h-10 flex justify-center px-4 mb-2 left-1/2 -translate-x-1/2 border -translate-y-6 border-[#191919] text-white font-bold rounded-full text-xl text-center bg-[#191919]">
+        <>
+            {/* USER INFO */}
+            {user && <div className="currentUser relative min-w-52 w-max h-10 flex justify-center px-4 left-1/2 -translate-x-1/2 border border-[#191919] text-white font-bold rounded-full text-xl text-center bg-[#191919]">
                 <Link className='my-auto' target='_blank' href={`https://instagram.com/${user.username}`}>@{user.username}</Link>
             </div>
             }
-            {loading ?
-                <div className="relative min-w-full h-full !z-50 flex items-center gap-4 justify-evenly p-5" >
-                    {Array.from({ length: 6 }).map((_, index) => (
-                        <Skeleton key={index} className="w-[300px] max-h-[384px] min-h-[300px] rounded-md bg-indigo-100 drop-shadow-custom border border-black" />
-                    ))}
-                </div>
-                :
-                posts.map((post, index) => (
-                    <div key={index} className={cn(
-                        styles.instaPost,
-                        'flex flex-col relative px-4 self-center drop-shadow-custom h-[90%]',
-                        // { 'scale:.8': isMobile },
-                    )}>
-                        {/* <Card.Root className='rounded-3xl overflow-hidden w-full h-full md:!py-8 xsm:py-2 px-1 space-y-1'> */}
-                        <Card.Root className='rounded-3xl w-full h-full md:!pt-2 md:!pb-4 xsm:py-2 space-y-1'>
-                            <Card.Post
-                                post={post}
-                                index={index}
-                                className={cn(
-                                    styles.instaMidia,
-                                    'relative top-2 w-1/2',
-                                    '!max-h-[335px]'
-                                )} />
-                            <Card.Caption className='self-end' text={post.caption!} timestamp={post.timestamp!} />
-                        </Card.Root>
-                    </div >
-                ))
-            }
-        </section >
+            {/* POSTS */}
+            <section className={cn(
+                'w-full h-full flex overflow-x-auto gap-4 py-4',
+                styles.instaPostsWrapper,
+                className
+            )}
+            >
+                {loading ?
+                    <div className="relative min-w-full h-full !z-50 flex items-center gap-4 justify-evenly p-5" >
+                        {Array.from({ length: 6 }).map((_, index) => (
+                            <Skeleton key={index} className="w-[300px] max-h-[384px] min-h-[300px] rounded-md bg-indigo-100 drop-shadow-custom border border-black" />
+                        ))}
+                    </div>
+                    :
+                    posts.map((post, index) => (
+                        <div key={index} className={cn(
+                            styles.instaPost,
+                            'relative px-4 self-start drop-shadow-custom h-[90%]',
+                            // { 'scale:.8': isMobile },
+                        )}>
+                            {/* <Card.Root className='rounded-3xl overflow-hidden w-full h-full md:!py-8 xsm:py-2 px-1 space-y-1'> */}
+                            <Card.Root className='rounded-3xl w-full max-h-[34rem] min-h-[30rem] h-max flex flex-col justify-around'>
+                                <Card.Post
+                                    post={post}
+                                    index={index}
+                                    className={cn(
+                                        styles.instaMidia,
+                                        'relative w-1/2',
+                                        '!max-h-[335px]'
+                                    )} />
+                                <Card.Caption className='self-end max-h-[120px]' text={post.caption!} timestamp={post.timestamp!} />
+                            </Card.Root>
+                        </div >
+                    ))
+                }
+            </section >
+        </>
     );
 };
 
