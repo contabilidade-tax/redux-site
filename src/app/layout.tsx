6478430db5d8511b514c597179fd64121d9b3330
José <josepsousa2012@gmail.com>
2023-12-27 10:09:02 -0300

    fix iphone trabalhe-conosco bug & add meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,18 +76,30 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <MobileContextProvider>
       <LoadingProvider>
         <html lang="pt-BR">
-          {/* <div id='gtag'>
-            <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TAG}`}></Script>
-            <Script id='google-analytics'>
-              {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', ${process.env.NEXT_PUBLIC_GA_TAG});
-              `}
-            </Script>
-          </div> */}
+          {/* <!-- Meta Pixel Code --> */}
+          <Script id='meta-pixel'>
+            {
+              `
+              !function(f,b,e,v,n,t,s)
+              {
+                if(f.fbq) return; 
+                n=f.fbq=function() {
+                  n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+                  if (!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0';
+                  n.queue=[]; t=b.createElement(e); t.async=!0;
+                  t.src=v; s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)
+                }
+                (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', ${process.env.NEXT_PUBLIC_FB_PIXEL_ID});
+                fbq('track', 'PageView');
+                <noscript>
+                  <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1"/>
+                </noscript>
+              `
+            }
+          </Script>
+          {/* <!-- End MetaPixelCode--> */}
           {/* <body className={inter.className}> */}
           <body className={`${montserrat.variable} flex justify-between flex-col min-h-screen items-center`}>
             <Header />
--- src/components/Email/index.tsx
+++ src/components/Email/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Input } from '../ui/input'
 import { EmailProps } from '@/types'
-import { Tailwind, Container, Heading, Link, Text, Html, Head, Preview, Section, Row, Column, Img, Body, Hr } from "@react-email/components";
+import { Container, Heading, Link, Text, Html, Head, Preview, Section, Row, Column, Img, Body, Hr } from "@react-email/components";
 import { render as Render } from '@react-email/render';
 import * as React from 'react';
 
@@ -13,131 +13,216 @@ export function render(data: any) {
 
 export default function Email({ name, cidade, email, message, estado, whatsapp }: EmailProps) {
 
+  const rowStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '8px',
+  }
+
+  const columnStyle = {
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderColor: 'rgba(0, 0, 0, 0.7)',
+    color: 'black',
+    minWidth: '250px',
+    fontWeight: 'bold',
+  }
+
+  const footerColumn = {
+    marginBottom: '24px',
+    flex: '1',
+    width: '280px',
+  }
+  const footerHeading = {
+    fontSize: '18px',
+    lineHeight: '28px',
+    fontWeight: 'bold',
+    marginBottom: '8px',
+  }
+
   return (
     <Html lang='pt-BR'>
       <Head />
       <Preview>{name}, enviou seu currículo para trabalhar conosco através do site!</Preview>;
-      <Tailwind
-        config={{
-          theme: {
-            extend: {
-              colors: {
-                borderBodyColor: "rgb(176 190 197 / 1)",
-              },
-            },
-          },
-        }}>
-        <Body className='font-sans bg-white mx-auto'>
-          <Container className="flex h-full p-[24px]">
-            <Section className="w-[550px] border-2 border-solid border-gray-600/80 flex flex-col gap-[16px]">
-              {/* HEADER */}
-              <Container className="flex justify-center p-[8px] mx-auto">
-                <Row align='center'>
-                  <Column align='center'>
-                    <Img
-                      src="https://redux.app.br/assets/img/logo-verde-cortada.png"
-                      alt="Logo TAX"
-                      width={280}
-                      height={110}
-                      className="object-center object-contain" />
-                  </Column>
-                </Row>
+      <Body className='font-sans bg-white mx-auto'>
+        <Container style={{
+          display: 'flex',
+          height: '100%',
+          padding: '24px',
+        }}
+        >
+          <Section style={{
+            width: '550px',
+            borderWidth: '2px',
+            borderStyle: 'solid',
+            borderColor: 'rgba(96, 96, 96, 0.8)',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '16px',
+          }}
+          >
+            {/* HEADER */}
+            <Container style={{
+              display: 'flex',
+              justifyContent: 'center',
+              padding: '8px',
+              margin: 'auto',
+            }}
+            >
+              <Row align='center'>
+                <Column align='center'>
+                  <Img
+                    src="https://redux.app.br/assets/img/logo-verde-cortada.png"
+                    alt="Logo TAX"
+                    width={280}
+                    height={110}
+                    style={{
+                      objectFit: 'contain',
+                      objectPosition: 'center',
+                    }}
+                  />
+                </Column>
+              </Row>
+            </Container>
+            {/* BODY */}
+            <Container style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              flex: '1',
+              padding: '16px',
+              gap: '16px',
+            }}
+            >
+              <Heading as='h1' style={{
+                fontSize: '36px',
+                lineHeight: '40px',
+                textAlign: 'center',
+              }}
+              ><span style={{ fontWeight: 'bold' }}>{name}</span>, enviou seu currículo para trabalhar conosco através do site!</Heading>
+              <Container style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'start',
+                gap: '24px',
+                padding: '24px',
+                borderWidth: '2px',
+                borderStyle: 'solid',
+                borderColor: 'rgba(176, 190, 197, 1)',
+              }}
+              >
+                <Heading as='h2' style={{
+                  fontSize: '20px',
+                  lineHeight: '28px',
+                  alignSelf: 'start',
+                  fontWeight: 'bold',
+                  color: 'rgb(213, 161, 21)',
+                }}>
+                  Resumo de seus dados:
+                </Heading>
+                {/* FIELDS */}
+                <Section style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '12px',
+                }}>
+                  <Row style={rowStyle}>
+                    <Column align='left'>
+                      <Text>Nome: </Text>
+                    </Column>
+                    <Column align='right'>
+                      <Input type="text" placeholder="Nome" style={columnStyle} disabled={true} defaultValue={name} />
+                    </Column>
+                  </Row>
+                  <Row style={rowStyle}>
+                    <Column align='left'>
+                      <Text>Email: </Text>
+                    </Column>
+                    <Column align='right'>
+                      <Input type="text" placeholder="email" style={columnStyle} disabled={true} defaultValue={email} />
+                    </Column>
+                  </Row>
+                  <Row style={rowStyle}>
+                    <Column align='left'>
+                      <Text>Whatsapp: </Text>
+                    </Column>
+                    <Column align='right'>
+                      <Input type="text" placeholder="Whatsapp" style={columnStyle} disabled={true} defaultValue={whatsapp} />
+                    </Column>
+                  </Row>
+                  <Row style={rowStyle}>
+                    <Column align='left'>
+                      <Text>Cidade: </Text>
+                    </Column>
+                    <Column align='right'>
+                      <Input type="text" placeholder="Cidade" style={columnStyle} disabled={true} defaultValue={cidade} />
+                    </Column>
+                  </Row>
+                  <Row style={rowStyle}>
+                    <Column align='left'>
+                      <Text>Estado: </Text>
+                    </Column>
+                    <Column align='right'>
+                      <Input type="text" placeholder="Estado" style={columnStyle} disabled={true} defaultValue={estado} />
+                    </Column>
+                  </Row>
+                  <Row style={rowStyle}>
+                    <Column align='left'>
+                      <Text>Mensagem: </Text>
+                    </Column>
+                    <Column align='right'>
+                      <Input type="text" placeholder="Mensagem" style={columnStyle} disabled={true} defaultValue={message} />
+                    </Column>
+                  </Row>
+                </Section>
               </Container>
-              {/* BODY */}
-              <Container className="flex flex-col justify-between items-center flex-1 px-[16px] gap-[16px]">
-                <Heading as='h1' className="text-[36px] leading-[40px] text-center"><span className="font-bold">{name}</span>, enviou seu currículo para trabalhar conosco através do site!</Heading>
-                <Container className='flex flex-col justify-start gap-[24px] p-[24px] border-2 border-solid border-borderBodyColor'>
-                  <Heading as='h2' className='text-[20px] leading-[28px] self-start font-semibold text-primary-color'>Resumo de seus dados: </Heading>
-                  {/* FIELDS */}
-                  <Section className='dataFields flex flex-col gap-[12px]'>
-                    <Row className='email flex items-center gap-[8px]'>
-                      <Column align='left'>
-                        <Text>Nome: </Text>
-                      </Column>
-                      <Column align='right'>
-                        <Input type="text" placeholder="Nome" className='border border-solid border-black/70 text-black min-w-[250px] font-semibold' disabled={true} defaultValue={name} />
-                      </Column>
-                    </Row>
-                    <Row className='email flex items-center gap-[8px]'>
-                      <Column align='left'>
-                        <Text>Email: </Text>
-                      </Column>
-                      <Column align='right'>
-                        <Input type="text" placeholder="email" className='border border-solid border-black/70 text-black min-w-[250px] font-semibold' disabled={true} defaultValue={email} />
-                      </Column>
-                    </Row>
-                    <Row className='email flex items-center gap-[8px]'>
-                      <Column align='left'>
-                        <Text>Whatsapp: </Text>
-                      </Column>
-                      <Column align='right'>
-                        <Input type="text" placeholder="Whatsapp" className='border border-solid border-black/70 text-black min-w-[250px] font-semibold' disabled={true} defaultValue={whatsapp} />
-                      </Column>
-                    </Row>
-                    <Row className='email flex items-center gap-[8px]'>
-                      <Column align='left'>
-                        <Text>Cidade: </Text>
-                      </Column>
-                      <Column align='right'>
-                        <Input type="text" placeholder="Cidade" className='border-2 border-solid border-black/70 text-black min-w-[250px] font-semibold' disabled={true} defaultValue={cidade} />
-                      </Column>
-                    </Row>
-                    <Row className='email flex items-center gap-[8px]'>
-                      <Column align='left'>
-                        <Text>Estado: </Text>
-                      </Column>
-                      <Column align='right'>
-                        <Input type="text" placeholder="Estado" className='border-2 border-solid border-black/70 text-black min-w-[250px] font-semibold' disabled={true} defaultValue={estado} />
-                      </Column>
-                    </Row>
-                    <Row className='email flex items-center gap-[8px]'>
-                      <Column align='left'>
-                        <Text>Mensagem: </Text>
-                      </Column>
-                      <Column align='right'>
-                        <Input type="text" placeholder="Mensagem" className='border-2 border-solid border-black/70 text-black min-w-[250px] font-semibold' disabled={true} defaultValue={message} />
-                      </Column>
-                    </Row>
-                  </Section>
-                </Container>
-              </Container>
-              <Hr />
-              {/* FOOTER  */}
-              <Container>
-                <Row className="bg-gray-200 p-[20px] flex justify-between text-gray-800">
-                  <Column className="mb-[24px] flex-1 max-w-[300px]">
-                    <Heading as='h3' className="text-[18px] leading-[28px] font-semibold mb-[8px]">Sobre</Heading>
-                    <Text className="text-[14px] leading-[20px]">Este e-mail é uma comunicação automática gerada no processo de candidatura de um interessado na empresa  através da seção "Trabalhe Conosco" no site da Redux.</Text>
-                  </Column>
-                  <Column className="mb-[24px] flex-1">
-                    <Heading as='h3' className="text-[18px] leading-[28px] font-[600] mb-[8px]">Contato</Heading>
-                    <Text className="text-[12px] leading-[16px]">Av. Virgílio Távora, 11 - Fátima, Juazeiro do Norte-CE</Text>
-                    <Text className="text-[12px] leading-[16px]">programacao@contabilidade-tax.com.br</Text>
-                  </Column>
-                  <Column className='mb-[24px] w-[120px] mx-auto'>
-                    <Heading as='h3' className="text-[18px] leading-[28px] font-semibold mb-[8px]">Links Úteis</Heading>
-                    <Row className='flex flex-col'>
-                      <Column>
-                        <Link href="https://redux.app.br/home" className="mb-[4px] text-[14px] leading-[20px] hover:underline">Home</Link>
-                      </Column>
-                    </Row>
-                    <Row>
-                      <Column>
-                        <Link href="https://redux.app.br/home#servicos" className="mb-[4px] text-[14px] leading-[20px] hover:underline">Serviços</Link>
-                      </Column>
-                    </Row>
-                    <Row>
-                      <Column>
-                        <Link href="https://redux.app.br/trabalhe-conosco" className="text-[14px] leading-[20px] hover:underline">Trabalhe conosco</Link>
-                      </Column>
-                    </Row>
-                  </Column>
-                </Row>
-              </Container>
-            </Section>
-          </Container>
-        </Body>
-      </Tailwind>
+            </Container>
+            <Hr />
+            {/* FOOTER  */}
+            <Container>
+              <Row style={{
+                backgroundColor: 'rgba(192, 192, 192, 1)',
+                padding: '20px',
+                display: 'flex',
+                justifyContent: 'space-between',
+                color: 'rgba(128, 128, 128, 1)',
+              }}>
+                <Column style={footerColumn}>
+                  <Heading as='h3' style={footerHeading}>Sobre</Heading>
+                  <Text style={{
+                    fontSize: '14px',
+                    lineHeight: '20px',
+                  }}>Este e-mail é uma comunicação automática gerada no processo de candidatura de um interessado na empresa  através da seção "Trabalhe Conosco" no site da Redux.</Text>
+                </Column>
+                <Column style={{ marginBottom: '24px', width: '150px', }}>
+                  <Heading as='h3' style={footerHeading}>Contato</Heading>
+                  <Text style={{ fontSize: '12px', lineHeight: '16px' }}>Av. Virgílio Távora, 11 - Fátima, Juazeiro do Norte-CE</Text>
+                  <Text style={{ fontSize: '12px', lineHeight: '16px' }}>programacao@contabilidade-tax.com.br</Text>
+                </Column>
+                <Column style={{ marginBottom: '24px', width: '120px', margin: 'auto' }}>
+                  <Heading as='h3' style={footerHeading}>Links Úteis</Heading>
+                  <Row style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Column>
+                      <Link href="https://redux.app.br/home" style={{ marginBottom: '4px', fontSize: '14px', lineHeight: '20px' }} className='links'>Home</Link>
+                    </Column>
+                  </Row>
+                  <Row>
+                    <Column>
+                      <Link href="https://redux.app.br/home#servicos" style={{ marginBottom: '4px', fontSize: '14px', lineHeight: '20px' }}>Serviços</Link>
+                    </Column>
+                  </Row>
+                  <Row>
+                    <Column>
+                      <Link href="https://redux.app.br/trabalhe-conosco" style={{ marginBottom: '4px', fontSize: '14px', lineHeight: '20px' }}>Trabalhe conosco</Link>
+                    </Column>
+                  </Row>
+                </Column>
+              </Row>
+            </Container>
+          </Section>
+        </Container>
+      </Body>
     </Html>
   )
 }
