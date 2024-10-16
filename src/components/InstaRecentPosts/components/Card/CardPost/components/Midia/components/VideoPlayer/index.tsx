10c807912e0f2358e127a143dbb8958e615c88f0
José Pinheiro <josepsousa2012@gmail.com>
2024-10-16 16:11:20 -0300

    new video component with preload and more smart

--- src/components/InstaRecentPosts/components/Card/CardPost/components/Midia/components/VideoPlayer/index.tsx
+++ src/components/InstaRecentPosts/components/Card/CardPost/components/Midia/components/VideoPlayer/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { VideoPlayerProps } from "@/types";
 import { Volume, VolumeX } from "lucide-react";
 import { cn } from "@/lib/utils";
@@ -12,19 +12,52 @@ export default function VideoPlayer({
   ...rest
 }: VideoPlayerProps) {
   const [isMuted, setIsMuted] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
   const handleVideoClick = () => {
     setIsMuted(!isMuted);
   };
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsPlaying(true);
+            if (videoRef.current) {
+              videoRef.current.play(); // Inicia a reprodução
+            }
+          } else {
+            setIsPlaying(false);
+            if (videoRef.current) {
+              videoRef.current.pause(); // Pausa a reprodução
+            }
+          }
+        });
+      },
+      { threshold: 0.5 } // 50% do vídeo deve estar visível para considerar 'intersectando'
+    );
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="relative h-full w-full">
       <video
+        ref={videoRef} // Referência ao vídeo
         className={cn(className, "rounded-2xl")}
-        autoPlay
         muted={isMuted}
         loop
-        onClick={handleVideoClick}
+        preload="metadata" // Configuração de preload
         playsInline={true}
         {...rest}
       >
@@ -34,14 +67,14 @@ export default function VideoPlayer({
       {isMuted ? (
         <VolumeX
           onClick={handleVideoClick}
-          className="absolute bottom-4 left-4 text-white"
+          className="absolute bottom-4 left-4 text-white cursor-pointer"
           fill="#202022"
           size={30}
         />
       ) : (
         <Volume
           onClick={handleVideoClick}
-          className="absolute bottom-4 left-4 text-white"
+          className="absolute bottom-4 left-4 text-white cursor-pointer"
           fill="#202022"
           size={30}
         />
