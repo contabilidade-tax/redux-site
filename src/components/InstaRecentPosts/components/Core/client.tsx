9f0a76a171c6ad898c310ca4f6222d40924783f7
José Pinheiro <josepsousa2012@gmail.com>
2024-10-16 23:09:11 -0300

    remove videos from instaposts on mobile devices

--- src/components/InstaRecentPosts/components/Core/client.tsx
+++ src/components/InstaRecentPosts/components/Core/client.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import { Card } from "../Card";
 import Link from "next/link";
@@ -7,7 +8,6 @@ import {
   Virtual,
   Navigation,
   Pagination,
-  Keyboard,
   Scrollbar,
 } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -21,9 +21,39 @@ import "swiper/css";
 
 type Props = {
   posts?: InstaPostData[];
+  userAgent: string | null;
 };
 
-export default function PostsCoreContent({ posts }: Props) {
+export default function PostsCoreContent({ posts, userAgent }: Props) {
+  const [isMobile, setIsMobile] = useState(false)
+  const [filteredPosts, setFilteredPosts] = useState<InstaPostData[] | undefined>(posts);
+
+  const checkUserAgent = (userAgent: string) => {
+    const mobileUserAgents = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.browser|up\.link|vodafone|wap|windows ce|xda|xiino/i;
+
+    const legacyMobileUserAgents = /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|acer|aos|airn|alav|alca|amoi|anex|anny|aptu|arch|argo|aste|asus|attw|audi|avan|beck|bell|bumb|bw-n|bwu|c55|cap|ccwa|cdm|cell|chtm|cldc|cmd|craw|dait|dbte|dc-s|devi|dica|dmob|doco|ds12|el49|elai|eml2|eric|esl8|ez[4-7]0|fetc|fly|g1 u|g560|gene|gf-5|g-mo|good|grad|haie|hei|hipt|hp ip|hs-c|htc|hua|i-go|i230|iac|idea|ig01|ikom|inno|ipaq|iris|jbro|jemu|jigs|kddi|keji|lg g|libw|lynx|maeo|miat|mmef|mot|mt50|n20[2-3]|ne|nok|opwv|owg1|p800|pan|pdxg|phil|play|pire|prox|psio|qtek|r380|r600|rim9|sama|sc01|sdk\/|sie-m|slid|son|sp01|telm|toapl|tx-9|veri|voda|webc|whit|wonu|zte-/i;
+
+    return mobileUserAgents.test(userAgent) || legacyMobileUserAgents.test(userAgent);
+  };
+
+
+
+  useEffect(() => {
+    if (userAgent) {
+      setIsMobile(checkUserAgent(userAgent))
+    }
+  }, [userAgent])
+
+  useEffect(() => {
+    if (posts) {
+      setFilteredPosts(
+        isMobile
+          ? posts.filter(post => post.media_type !== "VIDEO")
+          : posts
+      )
+    }
+  }, [isMobile, posts]);
+
   return (
     <Swiper
       modules={[Scrollbar, Navigation, Pagination]}
@@ -34,8 +64,8 @@ export default function PostsCoreContent({ posts }: Props) {
       rewind
       className="flex !h-full !w-full !max-w-[1450px]"
     >
-      {posts
-        ? posts.map((post, index) => (
+      {filteredPosts
+        ? filteredPosts.map((post, index) => (
           <SwiperSlide
             className="!m-0 !grid !h-fit !w-fit !place-items-center space-y-4 !p-0"
             key={index}
--- src/components/InstaRecentPosts/components/Core/index.tsx
+++ src/components/InstaRecentPosts/components/Core/index.tsx
@@ -1,7 +1,12 @@
+import { headers } from "next/headers";
 import { fetchPosts } from "../../actions/fetchOrUpdatePosts";
 import PostsCoreContent from "./client";
 
 export default async function PostsCoreContentServer() {
+  const headersList = headers();
+  const userAgent = headersList.get('user-agent');
+
   const posts = await fetchPosts();
-  return <PostsCoreContent posts={posts} />;
+
+  return <PostsCoreContent posts={posts} userAgent={userAgent} />;
 }
